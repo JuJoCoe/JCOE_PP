63ae7b27064ebcc5065a2b6158734e10338fbde5
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-03-03 16:44:50 -0600

    Update test.c

--- PP1/Q1/test.c
+++ PP1/Q1/test.c
@@ -2,14 +2,15 @@
 #include <stdio.h>
 #include <limits.h>
 #include <stdlib.h>
+#define MAX_THREADS 16
 
  void *find_min(void *list_ptr);
  pthread_mutex_t minimum_value_lock;
  int minimum_value, partial_list_size;
  int list_size = 100000000;
  int list_ptr[100000000];
-
-
+ int num_threads = MAX_THREADS;
+ int bounds = 0;
 
 
   int
@@ -26,23 +27,52 @@
   pthread_mutex_unlock (
   pthread_mutex_t *mutex_lock);
 
- main() {
-	 srand(time(0));
+ int main(int argc, char **argv) {
+	 pthread_t p_threads[MAX_THREADS];
+	  	pthread_attr_t attr;
+	  	pthread_attr_init(&attr);
+	// srand(time(0));
 	 for(int i = 0; i < list_size; i++){
 		 list_ptr[i] = rand();
 	 }
+
+	 if (argc != 2) {
+	 		printf("Usage: %s <number of threads>\n", argv[0]);
+	 		printf("Using a default number of threads: %d\n", MAX_THREADS);
+	 		num_threads = MAX_THREADS;
+	 	} else {
+	 		num_threads = atoi(argv[1]);
+	 		if (!num_threads || num_threads > MAX_THREADS) {
+	 			printf("Incorrect number of threads provided\n");
+	 			printf("Using a default number of threads: %d\n", MAX_THREADS);
+	 			num_threads = MAX_THREADS;
+	 		}
+	 	}
+
  /* declare and initialize data structures and list */
  minimum_value = INT_MAX;
  //pthread_init();
  pthread_mutex_init(&minimum_value_lock, NULL);
- find_min(list_ptr);
+ partial_list_size = list_size/num_threads;
+ for (int i = 0; i < num_threads; i++) {
+ 		pthread_create(&p_threads[i], &attr, find_min, &list_ptr[bounds]);
+ 		bounds = bounds + partial_list_size;
+ 	}
+
+ for (int i = 0; i < num_threads; i++) {
+ 		pthread_join(p_threads[i], NULL);
+ 	}
+
+ printf("Total Min = %d\n", minimum_value);
+
+ //find_min(list_ptr);
 
  /* initialize lists, list_ptr, and partial_list_size */
  /* create and join threads here */
  }
 
  void *find_min(void *list_ptr) {
- partial_list_size = list_size;
+
  int *partial_list_pointer, my_min, i;
  my_min = INT_MAX;
  partial_list_pointer = (int *) list_ptr;
@@ -61,3 +91,4 @@
  printf("my_min = %d\n", my_min);
  pthread_exit(0);
 }
+

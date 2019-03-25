0d69362bd88d1c5989f5d560ec41e916beb28095
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-03-25 16:08:19 -0500

    Create example7_14_2.c

---
+++ PP2/example7_14_2.c
@@ -0,0 +1,71 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <omp.h>
+#include <time.h>
+#include <limits.h>
+#define MAX_THREADS 8
+
+int i, j, k;
+int dim;
+int num_threads = MAX_THREADS;
+
+float a[1000][1000];
+float b[1000][1000];
+float c[1000][1000];
+
+
+
+int main(int argc, char **argv)
+{
+
+	//Takes in the argument of the number of threads they want
+	 if (argc != 2) {
+ 	 		printf("Usage: %s <number of threads>\n", argv[0]);
+ 	 		printf("Using a default number of threads: %d\n", MAX_THREADS);
+  	 		num_threads = MAX_THREADS;
+	 } else {
+  	 		num_threads = atoi(argv[1]);
+  	 		if (!num_threads || num_threads > MAX_THREADS) {
+  	 			printf("Incorrect number of threads provided\n");
+  	 			printf("Using a default number of threads: %d\n", MAX_THREADS);
+  	 			num_threads = MAX_THREADS;
+  	 		}
+  	 	}
+
+	//Size of dimensions
+	dim = 500;
+
+	//Populates the 2 arrays using random integers.
+	//Takes random integers between -5 and 5 for consistency and gets rid of all 0 values
+	//because they make complicate multiplication
+		 srand(time(0));
+		 //srand(1);
+		for(int i = 0; i < dim; i++){
+			 for(int j = 0; j < dim; j++){
+				 a[i][j] = (rand() % 11) - 5;
+			 			if(a[i][j] == 0){
+			 				a[i][j] = 1;
+
+			 			}
+
+			 	b[i][j] = (rand() % 11) - 5;
+			 		 	 if(b[i][j] == 0){
+			 			 b[i][j] = 1;
+			 		 }
+			 		}
+			 	 }
+
+
+	//Matrix multiplication algorithm received from the test book
+
+	for (i = 0; i < dim; i++) {
+		//Parallelize inner 2 loops using collapse method
+		#pragma omp parallel for shared(a, b, c, dim) num_threads(num_threads) schedule(static, 16) collapse(2)
+		for (j = 0; j < dim; j++) {
+			for (k = 0; k < dim; k++) {
+				 c[i][j] += a[i] [k] * b[k][j];
+
+			}
+		}
+	}
+}

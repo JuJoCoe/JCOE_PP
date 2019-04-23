140559d19bb52d0a15aed37a0fb4999f1a70a2d9
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-04-23 11:20:26 -0500

    Update mpi_gaussian_elimination.c

--- PP3/mpi_gaussian_elimination.c
+++ PP3/mpi_gaussian_elimination.c
@@ -118,7 +118,9 @@ int main(int argc, char *argv[]) {
 	  }
 	  else {  // receive my part of
 		MPI_Recv(&number, 1, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	    if(number == 1){
 	    MPI_Recv(A[0], stripSize * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		}
 	    printf("number = %d from node %s, rank %d\n", number, processor_name, myrank);
 	    MPI_Barrier(MPI_COMM_WORLD);
 

88ac5e1c881f5b2fcb259ec6844a40cab05277f1
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-04-24 16:45:35 -0500

    Update test.c

--- PP3/test.c
+++ PP3/test.c
@@ -96,6 +96,7 @@ int main(int argc, char *argv[]) {
 	for(i = 0; i < 1; i++){
 		for(j=0; j < 1; j++){
 			A[i][j] = k;
+			printf("A[%d][%d] = %f from node %s, rank %d\n", i, j, A[i][j], processor_name, myrank);
 			}
 		}
 	}

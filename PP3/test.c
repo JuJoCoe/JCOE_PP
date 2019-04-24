8ee54a58f28a935589e4da1a87818d1f86b72f22
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-04-24 16:43:15 -0500

    Update test.c

--- PP3/test.c
+++ PP3/test.c
@@ -85,13 +85,11 @@ int main(int argc, char *argv[]) {
 	    for (i=1; i<numnodes; i++) {
 	      MPI_Send(A[offset], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
 	      offset += stripSize;
-		    MPI_Barrier(MPI_COMM_WORLD);
 	      
 	    }
 	  }
 	  else {  // receive my part of A
 	    MPI_Recv(A[0], stripSize * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		  MPI_Barrier(MPI_COMM_WORLD);
 	  }
 
 	if(myrank != 0){	
@@ -105,12 +103,10 @@ int main(int argc, char *argv[]) {
 		 for (i=1; i<numnodes; i++) {
 			MPI_Recv(A[offset], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			offset += stripSize;
-			 MPI_Barrier(MPI_COMM_WORLD);
 			
 		 }
 	}else{
 		MPI_Send(A[0], stripSize * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD);
-		MPI_Barrier(MPI_COMM_WORLD);
 	}
 
 	if(myrank == 0){

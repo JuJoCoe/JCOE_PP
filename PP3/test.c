1c5838e00c9258ce88adae37bfe7c506c5b0877f
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-01 19:10:44 -0500

    Update test.c

--- PP3/test.c
+++ PP3/test.c
@@ -217,7 +217,16 @@ int main(int argc, char *argv[]) {
 
 	
 
-
+	if(myrank == 0){
+	if(N < 10){
+		//Print outputs
+		printf("\n");
+		printf("X Outputs:\n");
+	//	for(int l = 0; l <= N-1; l++){
+	//		 printf("x%d = %f\n", l, x[l]);
+	//	}
+	}
+	}
 
 
 

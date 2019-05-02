029945d50ad180b68fd3a7d9c447e37c04ae6b8e
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-02 17:44:10 -0500

    Update mpi_gaussian_elimination.c

--- PP3/mpi_gaussian_elimination.c
+++ PP3/mpi_gaussian_elimination.c
@@ -12,7 +12,7 @@ int main(int argc, char **argv)
     int N=250;
     int myrank, numnodes;
     double startTime, endTime;
-    
+
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);   /* get current process id */
     MPI_Comm_size(MPI_COMM_WORLD, &numnodes); /* get number of processes */
@@ -20,10 +20,10 @@ int main(int argc, char **argv)
 
 //////////////////////////////////////////////////////////////////////////////////
 
-   if(rank == 0){
+   if(myrank == 0){
     srand(1);
-    for(int i = 0; i < n; i++){
-    	for(int j = 0; j < n; j++){ 	
+    for(int i = 0; i < N; i++){
+    	for(int j = 0; j < N; j++){
     	 	A[i][j] = (rand() % 11) - 5;
          	if(A[i][j] == 0){
     	 		A[i][j] = 1;
@@ -37,29 +37,29 @@ int main(int argc, char **argv)
    }
 
     MPI_Bcast (A,N*N,MPI_DOUBLE,0,MPI_COMM_WORLD);
-    MPI_Bcast (b,N,MPI_DOUBLE,0,MPI_COMM_WORLD);    
-    
+    MPI_Bcast (b,N,MPI_DOUBLE,0,MPI_COMM_WORLD);
+
     for(i=0; i<N; i++)
     {
         index[i]= i % numnodes;
-    } 
+    }
 
-    for(k=0;k<n;k++)
+    for(k=0;k<N;k++)
     {
         MPI_Bcast (&A[k][k],N-k,MPI_DOUBLE,index[k],MPI_COMM_WORLD);
         MPI_Bcast (&b[k],1,MPI_DOUBLE,index[k],MPI_COMM_WORLD);
-        for(i= k+1; i<n; i++) 
+        for(i= k+1; i<N; i++)
         {
             if(index[i] == myrank)
             {
                 multiplier[i]=A[i][k]/A[k][k];
             }
-        }               
-        for(i= k+1; i<n; i++) 
-        {       
+        }
+        for(i= k+1; i<N; i++)
+        {
             if(index[i] == myrank)
             {
-                for(j=0;j<n;j++)
+                for(j=0;j<N;j++)
                 {
                     A[i][j]=A[i][j]-( multiplier[i]*A[k][j] );
                 }
@@ -68,20 +68,19 @@ int main(int argc, char **argv)
         }
     }
 
-    if (myrank==0)
-    { 
-    x[n-1]=b[n-1]/A[n-1][n-1];
-    for(i=n-2;i>=0;i--)
-    {
+    if (myrank==0){
+
+    	x[N-1]=b[N-1]/A[N-1][N-1];
+    	for(i=N-2;i>=0;i--){
+
         float sum=0;
-        for(j=i+1;j<n;j++)
+        for(j=i+1;j<N;j++)
         {
             sum=sum+A[i][j]*x[j];
         }
         x[i]=(b[i]-sum)/A[i][i];
     }
-
-
+    }
     MPI_Finalize();
     return(0);
 

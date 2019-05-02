484a31e3dd530056db6163361a7f15afdc27b29e
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-02 16:06:08 -0500

    Update test.c

--- PP3/test.c
+++ PP3/test.c
@@ -76,6 +76,10 @@ int main(int argc, char *argv[]) {
 	}
 
 
+	if (myrank == 0) {
+    		startTime = MPI_Wtime();
+	  }
+
 	for(k = 0; k < N ; k++){
 
 		if (myrank == 0){
@@ -203,6 +207,11 @@ int main(int argc, char *argv[]) {
 
 	}
 
+
+	if (myrank == 0) {
+    endTime = MPI_Wtime();
+    printf("Time is %f\n", endTime-startTime);
+  }
 
 
 

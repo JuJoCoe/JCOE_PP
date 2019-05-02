eda7293401c53af49daf3969e69d1130343445d6
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-02 15:35:18 -0500

    Update test.c

--- PP3/test.c
+++ PP3/test.c
@@ -133,7 +133,7 @@ int main(int argc, char *argv[]) {
 
 		  }
 
-	if(number != 0){
+	if(number != 0 && myrank != 0){
 		for(int s = 0; s<(size/N); s++){
 			printf("k = %d\n" , k);
 			float z = A[s][k];

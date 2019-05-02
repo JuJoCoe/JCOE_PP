c8c6878557fe3e034a5ccfb37d46bcdfcc7afd3f
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-02 16:36:28 -0500

    Update mpi_gaussian_elimination.c

--- PP3/mpi_gaussian_elimination.c
+++ PP3/mpi_gaussian_elimination.c
@@ -1,156 +1,119 @@
-#include <mpi.h>
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <mpi.h>
 
-#define TAG 13
+int main(int argc, char **argv)
+{
+    MPI_Init(&argc, &argv);
 
-int main(int argc, char *argv[]) {
-	double **A, *b, *x, *tmp;
-	int map[3];
-	int C[3];
-	int N = 2000;
-	double startTime, endTime;
-	int myrank, numnodes, stripSize, offset, numElements;
-	int i, j, k;
-	int number = 0;
-	double y;
-	int indexrow;
+    int i,j,k;
+    int map[500];
+    float A[500][500],b[500],c[500],x[500],sum=0.0;
+    double range=1.0;
+    int n=3;
+    int rank, nprocs;
+    clock_t begin1, end1, begin2, end2;
+    MPI_Status status;
 
-	MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);   /* get current process id */
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs); /* get number of processes */
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	MPI_Comm_size(MPI_COMM_WORLD, &numnodes);
+//////////////////////////////////////////////////////////////////////////////////
 
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
+    if (rank==0)
+    {
+        for (i=0; i<n; i++)
+        {
+        for (j=0; j<n; j++)
+        A[i][j]=range*(1.0-2.0*(double)rand()/RAND_MAX);
+        b[i]=range*(1.0-2.0*(double)rand()/RAND_MAX);
+        }
+        printf("\n Matrix A (generated randomly):\n");
+        for (i=0; i<n; i++)
+        {
+            for (j=0; j<n; j++)
+            printf("%9.6lf ",A[i][j]);
+        printf("\n");
+        }
+        printf("\n Vector b (generated randomly):\n");
+        for (i=0; i<n; i++)
+            printf("%9.6lf ",b[i]);
+        printf("\n\n");
+    }
 
+//////////////////////////////////////////////////////////////////////////////////
 
-	N = atoi(argv[1]);
+    begin1 =clock();
 
-	//Allocate A
-	if (myrank == 0) {
-	    tmp = (double *) malloc (sizeof(double ) * N * N);
-	    A = (double **) malloc (sizeof(double *) * N);
-	    for (i = 0; i < N; i++)
-	      A[i] = &tmp[i * N];
-	  }
-	  else {
-	    tmp = (double *) malloc (sizeof(double ) * N * N / numnodes);
-	    A = (double **) malloc (sizeof(double *) * N / numnodes);
-	    for (i = 0; i < N / numnodes; i++)
-	      A[i] = &tmp[i * N];
-	  }
+    MPI_Bcast (A,n*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
+    MPI_Bcast (b,n,MPI_DOUBLE,0,MPI_COMM_WORLD);    
 
-	//Allocate b
-	b = (double *) malloc (sizeof(double ) * N);
-	for(i = 0; i < N; i++){
-		b[i] = 1.0;
-	}
+    for(i=0; i<n; i++)
+    {
+        map[i]= i % nprocs;
+    } 
 
+    for(k=0;k<n;k++)
+    {
+        MPI_Bcast (&A[k][k],n-k,MPI_DOUBLE,map[k],MPI_COMM_WORLD);
+        MPI_Bcast (&b[k],1,MPI_DOUBLE,map[k],MPI_COMM_WORLD);
+        for(i= k+1; i<n; i++) 
+        {
+            if(map[i] == rank)
+            {
+                c[i]=A[i][k]/A[k][k];
+            }
+        }               
+        for(i= k+1; i<n; i++) 
+        {       
+            if(map[i] == rank)
+            {
+                for(j=0;j<n;j++)
+                {
+                    A[i][j]=A[i][j]-( c[i]*A[k][j] );
+                }
+                b[i]=b[i]-( c[i]*b[k] );
+            }
+        }
+    }
+    end1 = clock();
 
-	//Allocate x
-	x = (double *) malloc (sizeof(double ) * N);
-		for(i = 0; i < N; i++){
-			x[i] = 0.0;
-		}
+//////////////////////////////////////////////////////////////////////////////////
 
-	if(myrank == 0){
-		A[0][0] = 2;
-		A[0][1] = 1;
-		A[0][2] = -1;
-		b[0] = 8;
+    begin2 =clock();
 
-		A[1][0] = -3;
-		A[1][1] = -1;
-		A[1][2] = 2;
-		b[1] = -11;
+    if (rank==0)
+    { 
+    x[n-1]=b[n-1]/A[n-1][n-1];
+    for(i=n-2;i>=0;i--)
+    {
+        sum=0;
 
-		A[2][0] = -2;
-		A[2][1] = 1;
-		A[2][2] = 2;
-		b[2] = -3;
-	}
+        for(j=i+1;j<n;j++)
+        {
+            sum=sum+A[i][j]*x[j];
+        }
+        x[i]=(b[i]-sum)/A[i][i];
+    }
 
-		b[0] = 8;
-		b[1] = -11;
-		b[2] = -3;
+    end2 = clock();
+    }
+//////////////////////////////////////////////////////////////////////////////////
+    if (rank==0)
+    { 
+        printf("\nThe solution is:");
+        for(i=0;i<n;i++)
+        {
+            printf("\nx%d=%f\t",i,x[i]);
 
+        }
 
+        printf("\n\nLU decomposition time: %f", (double)(end1 - begin1) / CLOCKS_PER_SEC);
+        printf("\nBack substitution time: %f\n", (double)(end2 - begin2) / CLOCKS_PER_SEC);
+    }
 
-	//	 begin1 =clock();
-
-		    MPI_Bcast (A[0],9,MPI_DOUBLE,0,MPI_COMM_WORLD);
-		    MPI_Bcast (b,N,MPI_DOUBLE,0,MPI_COMM_WORLD);
-
-		    for(i=0; i<N; i++)
-		    {
-		        map[i]= i % numnodes;
-		    }
-
-		    for(k=0;k<N;k++)
-		    {
-		        MPI_Bcast (&A[k][k],N-k,MPI_DOUBLE,map[k],MPI_COMM_WORLD);
-		        MPI_Bcast (&b[k],1,MPI_DOUBLE,map[k],MPI_COMM_WORLD);
-		        for(i= k+1; i<N; i++)
-		        {
-		            if(map[i] == myrank)
-		            {
-		                C[i]=A[i][k]/A[k][k];
-		            }
-		        }
-		        for(i= k+1; i<N; i++)
-		        {
-		            if(map[i] == myrank)
-		            {
-		                for(j=0;j<N;j++)
-		                {
-		                    A[i][j]=A[i][j]-( C[i]*A[k][j] );
-		                }
-		                b[i]=b[i]-( C[i]*b[k] );
-		            }
-		        }
-		    }
-		 //   end1 = clock();
-
-		//////////////////////////////////////////////////////////////////////////////////
-
-	//	    begin2 =clock();
-
-		    if (myrank==0)
-		    {
-		    x[N-1]=b[N-1]/A[N-1][N-1];
-		    for(i=N-2;i>=0;i--)
-		    {
-		        double sum=0.0;
-
-		        for(j=i+1;j<N;j++)
-		        {
-		            sum=sum+A[i][j]*x[j];
-		        }
-		        x[i]=(b[i]-sum)/A[i][i];
-		    }
-
-	//	    end2 = clock();
-		    }
-		//////////////////////////////////////////////////////////////////////////////////
-		    if (myrank==0){
-		        printf("\nThe solution is:");
-		        for(i=0;i<N;i++)
-		        {
-		            printf("\nx%d=%f\t",i,x[i]);
-
-		        }
-		    }
-
-
-
-
-
-
+    return(0);
 
     MPI_Finalize();
-	return 0;
-
-
 }

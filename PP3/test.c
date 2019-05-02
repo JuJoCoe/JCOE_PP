f065f87309dd6a8c6199fd93cea2a834d5c40fa8
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-01 20:44:12 -0500

    Update test.c

--- PP3/test.c
+++ PP3/test.c
@@ -126,7 +126,7 @@ int main(int argc, char *argv[]) {
 	    		MPI_Send(&number, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
 	    		MPI_Send(&size, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
 	    		MPI_Send(A[indexrow], size, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
-	    		MPI_Send(b[count], bsize, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
+	    		MPI_Send(&b[count], bsize, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
 	    		indexrow = indexrow + IterationsPerThread + leftover;
 	    		count++;
 	    	}else{
@@ -135,7 +135,7 @@ int main(int argc, char *argv[]) {
 	    		MPI_Send(&number, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
 	    		MPI_Send(&size, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
 	    		MPI_Send(A[0], 1, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
-	    		MPI_Send(b[count], 1, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD)
+	    		MPI_Send(&b[count], 1, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
 	    	}
 		    printf("Sent first time %d\n", k);
 	    	}
@@ -146,7 +146,7 @@ int main(int argc, char *argv[]) {
 		printf("number and size = %d and %d from node %s, rank %d\n", number, size, processor_name, myrank);
 		int bsize = size/3;
 	    MPI_Recv(A[0], size, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-	    MPI_Recv(b[0], size, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	    MPI_Recv(b[0], bsize, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			printf("Recieved first time %d\n", k);
 		  }
 

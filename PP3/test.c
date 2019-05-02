f2dfc689859a39a82af10450a8a84049c6cc1613
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-01 21:46:13 -0500

    Update test.c

--- PP3/test.c
+++ PP3/test.c
@@ -143,6 +143,7 @@ int main(int argc, char *argv[]) {
 	MPI_Barrier(MPI_COMM_WORLD);
 	if(number != 0){
 		for(int s = 0; s<(size/N); s++){
+			printf("b[0] = %f\n", b[0]);
 			float z = A[s][k];
 			for(int l = k+1; l<N; l++){
 				A[s][l] = A[s][l] - z*A[k][l];

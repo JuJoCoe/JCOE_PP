8548a6016ce33d5d0f441392bf3be4c662f55ef0
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-02 17:37:24 -0500

    Update mpi_gaussian_elimination.c

--- PP3/mpi_gaussian_elimination.c
+++ PP3/mpi_gaussian_elimination.c
@@ -5,19 +5,18 @@
 
 int main(int argc, char **argv)
 {
-    MPI_Init(&argc, &argv);
 
+    float A[500][500],b[500],multiplier[500],x[500];
     int i,j,k;
-    int map[500];
-    float A[500][500],b[500],c[500],x[500],sum=0.0;
-    double range=1.0;
+    int index[500];
     int n=250;
-    int rank, nprocs;
-    clock_t begin1, end1, begin2, end2;
+    int myrank, numnodes;
+    double startTime, endTime;
+    
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);   /* get current process id */
+    MPI_Comm_size(MPI_COMM_WORLD, &numnodes); /* get number of processes */
     MPI_Status status;
-
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);   /* get current process id */
-    MPI_Comm_size(MPI_COMM_WORLD, &nprocs); /* get number of processes */
 
 //////////////////////////////////////////////////////////////////////////////////
 
@@ -39,14 +38,12 @@ int main(int argc, char **argv)
 
 //////////////////////////////////////////////////////////////////////////////////
 
-    begin1 =clock();
-
     MPI_Bcast (A,n*n,MPI_DOUBLE,0,MPI_COMM_WORLD);
     MPI_Bcast (b,n,MPI_DOUBLE,0,MPI_COMM_WORLD);    
 
     for(i=0; i<n; i++)
     {
-        map[i]= i % nprocs;
+        index[i]= i % numnodes;
     } 
 
     for(k=0;k<n;k++)
@@ -55,36 +52,33 @@ int main(int argc, char **argv)
         MPI_Bcast (&b[k],1,MPI_DOUBLE,map[k],MPI_COMM_WORLD);
         for(i= k+1; i<n; i++) 
         {
-            if(map[i] == rank)
+            if(index[i] == myrank)
             {
                 c[i]=A[i][k]/A[k][k];
             }
         }               
         for(i= k+1; i<n; i++) 
         {       
-            if(map[i] == rank)
+            if(index[i] == rank)
             {
                 for(j=0;j<n;j++)
                 {
                     A[i][j]=A[i][j]-( c[i]*A[k][j] );
                 }
-                b[i]=b[i]-( c[i]*b[k] );
+                b[i]=b[i]-(c[i]*b[k]);
             }
         }
     }
-    end1 = clock();
 
 //////////////////////////////////////////////////////////////////////////////////
 
-    begin2 =clock();
 
-    if (rank==0)
+    if (myrank==0)
     { 
     x[n-1]=b[n-1]/A[n-1][n-1];
     for(i=n-2;i>=0;i--)
     {
-        sum=0;
-
+        float sum=0;
         for(j=i+1;j<n;j++)
         {
             sum=sum+A[i][j]*x[j];
@@ -92,21 +86,6 @@ int main(int argc, char **argv)
         x[i]=(b[i]-sum)/A[i][i];
     }
 
-    end2 = clock();
-    }
-//////////////////////////////////////////////////////////////////////////////////
-    if (rank==0)
-    { 
-        printf("\nThe solution is:");
-        for(i=0;i<n;i++)
-        {
-            printf("\nx%d=%f\t",i,x[i]);
-
-        }
-
-        printf("\n\nLU decomposition time: %f", (double)(end1 - begin1) / CLOCKS_PER_SEC);
-        printf("\nBack substitution time: %f\n", (double)(end2 - begin2) / CLOCKS_PER_SEC);
-    }
 
     MPI_Finalize();
     return(0);

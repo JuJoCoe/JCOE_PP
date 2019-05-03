b782bbec6c366ce49ff30426bafd705932782d24
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-03 14:52:01 -0500

    Update mpi_gaussian_elimination.c

--- PP3/mpi_gaussian_elimination.c
+++ PP3/mpi_gaussian_elimination.c
@@ -46,41 +46,49 @@ int main(int argc, char **argv)
 
     for(k=0;k<N;k++)
     {
+    	float y = A[k][k];
+    	for(int j = k+1; j < N; j++){
+    		A[k][j] = A[k][j]/y;
+    		}
+    		A[k][k] = 1.0;
+    		b[k] = b[k]/y;
         MPI_Bcast (&A[k][k],N-k,MPI_DOUBLE,index[k],MPI_COMM_WORLD);
         MPI_Bcast (&b[k],1,MPI_DOUBLE,index[k],MPI_COMM_WORLD);
-        for(i= k+1; i<N; i++)
+ /*       for(i= k+1; i<N; i++)
         {
             if(index[i] == myrank)
             {
                 multiplier[i]=A[i][k]/A[k][k];
             }
-        }
+        }*/
         for(i= k+1; i<N; i++)
         {
+        	float z = A[i][k];
             if(index[i] == myrank)
             {
                 for(j=0;j<N;j++)
                 {
-                    A[i][j]=A[i][j]-( multiplier[i]*A[k][j] );
+                	A[i][j] = A[i][j] - z*A[k][j];
+                				}
+
+                	b[i] = b[i] - A[i][k] * b[k];
+                	A[i][k] = 0.0;
                 }
-                b[i]=b[i]-(multiplier[i]*b[k]);
             }
         }
-    }
+
 
     if (myrank==0){
 
-    	x[N-1]=b[N-1]/A[N-1][N-1];
-    	for(i=N-2;i>=0;i--){
+    	for(int i=N-1; i >= 0; i--){
+    		x[i] = b[i];
+    		for(int j=N-1; j > i; j--){
+    			x[i] = x[i] - A[i][j] * x[j];
+    			}
+    			x[i] = x[i]/A[i][i];
+    			}
+    	}
 
-        float sum=0;
-        for(j=i+1;j<N;j++)
-        {
-            sum=sum+A[i][j]*x[j];
-        }
-        x[i]=(b[i]-sum)/A[i][i];
-    }
-    }
     MPI_Finalize();
     return(0);
 

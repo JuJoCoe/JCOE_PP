19b08b8a87232a92ec49c9af6e76f639eba2fdda
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-05 17:37:04 -0500

    Update test.c

--- PP3/test.c
+++ PP3/test.c
@@ -1,216 +1,101 @@
-#include <mpi.h>
-#include <stdio.h>
+#include<stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <mpi.h>
 
-#define TAG 13
+int main(int argc, char **argv)
+{
 
-int main(int argc, char *argv[]) {
-	double **A, *b, *x, *tmp;
-	int N = 2000;
-	double startTime, endTime;
-	int myrank, numnodes, stripSize, offset, numElements;
-	int i, j, k;
-	int number = 0;
-	double y;
-	int leftover;
-	int indexrow;
-	int size;
-	int count = 0;
+	double **A, **LocalA, *b, *x, *tmp;
+    int i,j,k;
+    int index[500];
+    int N=2000;
+    int myrank, numnodes, stripSize, offset, numElements;
+    double startTime, endTime;
 
-	MPI_Init(&argc, &argv);
+    MPI_Init(&argc, &argv);
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-	MPI_Comm_size(MPI_COMM_WORLD, &numnodes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+    MPI_Comm_size(MPI_COMM_WORLD, &numnodes);
 
-	char processor_name[MPI_MAX_PROCESSOR_NAME];
-	int name_len;
-	MPI_Get_processor_name(processor_name, &name_len);
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
 
+    N = atoi(argv[1]);
 
-	N = atoi(argv[1]);
+    //Allocate memory for matrix A (Memory allocation code received from Yong Chen)
+    	if (myrank == 0) {
+    	    tmp = (double *) malloc (sizeof(double ) * N * N);
+    	    A = (double **) malloc (sizeof(double *) * N);
+    	    if(tmp == NULL){
+    	    	printf("ERROR ALLOCATING tmp in rank 0");
+    	    	return -1;
+    	    }
+    	    if(A == NULL){
+    	    	printf("ERROR ALLOCATING A in rank 0");
+    	    	return -1;
+    	    }
 
-	//Allocate A
-	if (myrank == 0) {
-	    tmp = (double *) malloc (sizeof(double ) * N * N);
-	    A = (double **) malloc (sizeof(double *) * N);
-	    if(tmp == NULL){
-	    	printf("ERROR ALLOCATING");
-	    	 return -1;
-	    	    }
-	    if(A == NULL){
-	    	printf("ERROR ALLOCATING 2");
-	    	return -1;
-	    }
-	    for (i = 0; i < N; i++)
-	      A[i] = &tmp[i * N];
-	  }
-	  else {
-	    tmp = (double *) malloc (sizeof(double ) * ((N * N / numnodes)+1));
-	    A = (double **) malloc (sizeof(double *) * ((N / numnodes)+1));
-	    if(tmp == NULL){
-	   	    	    	printf("ERROR ALLOCATING 3");
-	   	    	    	return -1;
-	   	    	    }
-	    if(A == NULL){
-	    	printf("ERROR ALLOCATING 4");
-	    	return -1;
-	    	    }
-	    for (i = 0; i < N / numnodes; i++)
-	      A[i] = &tmp[i * N];
-	  }
-
-	//Allocate b
-	b = (double *) malloc (sizeof(double ) * N);
-	 if(b == NULL){
-		    	    	printf("ERROR ALLOCATING b");
-		    	    	return -1;
-		    	    }
-	for(i = 0; i < N; i++){
-		b[i] = 1.0;
-	}
+    	    for (i = 0; i < N; i++)
+    	      A[i] = &tmp[i * N];
+    	  }
+    		   tmp = (double *) malloc (sizeof(double ) * ((N * N / numnodes)+1));
+    		   LocalA = (double **) malloc (sizeof(double *) * ((N / numnodes)+1));
+    		   if(tmp == NULL){
+    		  	  	printf("ERROR ALLOCATING tmp in cluster %s", processor_name);
+    		  	   	return -1;
+    		  	}
+    		  	if(A == NULL){
+    		  		printf("ERROR ALLOCATING A in cluster %s", processor_name);
+    		  	    return -1;
+    		  	}
+    		  	for (i = 0; i < N / numnodes; i++)
+    		  	  A[i] = &tmp[i * N];
 
 
-	//Allocate x
-	x = (double *) malloc (sizeof(double ) * N);
-	 if(x == NULL){
-		    	    	printf("ERROR ALLOCATING x");
-		    	    	return -1;
-		    	    }
-		for(i = 0; i < N; i++){
-			x[i] = 0.0;
-		}
-
-	//Populate Matrix
-	if(myrank == 0){
-		srand(1);
-		for(int i = 0; i < N; i++){
-			for(int j = 0; j < N; j++){
-			 	A[i][j] = (rand() % 11) - 5;
-			 	if(A[i][j] == 0){
-			 		A[i][j] = 1;
-			 			}
-			 		 }
-			 		 b[i] = rand() % (10 + 1 - 0) + 0;
-			 		 if(b[i] == 0){
-			 			 b[i] = 1;
-			 		 }
-			 	 	 }
-
-		MPI_Bcast(&b[0], N, MPI_INT, 0, MPI_COMM_WORLD);
-	}
+    	//Allocate b to everyone
+    	b = (double *) malloc (sizeof(double ) * N);
+    	 if(b == NULL){
+    		 printf("ERROR ALLOCATING b in cluster %s", processor_name);
+    		 return -1;
+    		 }
+    	for(i = 0; i < N; i++){
+    		b[i] = 1.0;
+    	}
 
 
-	if (myrank == 0) {
-    		startTime = MPI_Wtime();
-	  }
-
-	for(k = 0; k < N ; k++){
-
-		if (myrank == 0){
-			y = A[k][k];
-			for(int j = k+1; j < N; j++){
-				A[k][j] = A[k][j]/y;
-			}
-			A[k][k] = 1.0;
-			b[k] = b[k]/y;
-			MPI_Bcast(&b[0], N, MPI_INT, 0, MPI_COMM_WORLD);
-		}
-
-
-
-
-	stripSize = N/numnodes;
-
-  // send each node its piece of A -- note could be done via MPI_Scatter
- 	 if (myrank == 0) {
-    		offset = stripSize;
-    		numElements = stripSize * N;
-    		for (i=1; i<numnodes; i++) {
-			indexrow = offset;
-			MPI_Send(&indexrow, 1, MPI_INT, i, TAG, MPI_COMM_WORLD);
-      			MPI_Send(A[offset], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD);
-      			offset += stripSize;
+    	//Allocate x to only rank 0
+    	if(myrank == 0){
+    	x = (double *) malloc (sizeof(double ) * N);
+    	 if(x == NULL){
+    		 printf("ERROR ALLOCATING x in cluster %s", processor_name);
+    		 return -1;
+    		 }
+    		for(i = 0; i < N; i++){
+    			x[i] = 0.0;
     		}
-  	}
-  	else {  // receive my part of A
-		MPI_Recv(&indexrow, 1, MPI_INT, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    		MPI_Recv(A[0], stripSize * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-  	}
-
-	for(int s = 0; s<stripSize; s++){
-		float z = A[s][k];
-		for(int l = k+1; l<N; l++){
-			A[s][l] = A[s][l] - z*A[s][l];
-			//printf("Printed HERE\n");
-		}
-			b[s] = b[s] - A[s][k] * b[k];
-			A[s][k] = 0.0;
-		}
+    	}
 
 
-		
 
-if (myrank == 0) {
-   offset = stripSize;
-   numElements = stripSize * N;
-   for (i=1; i<numnodes; i++) {
-     MPI_Recv(A[offset], numElements, MPI_DOUBLE, i, TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-     offset += stripSize;
+   if(myrank == 0){
+    srand(1);
+    for(int i = 0; i < N; i++){
+    	for(int j = 0; j < N; j++){
+    	 	A[i][j] = (rand() % 11) - 5;
+         	if(A[i][j] == 0){
+    	 		A[i][j] = 1;
+    			}
+    		 }
+    		 b[i] = rand() % (10 + 1 - 0) + 0;
+    		 if(b[i] == 0){
+    	    	 b[i] = 1;
+    		 }
+    	 }
    }
- }
- else { // send my contribution to C
-   MPI_Send(A[0], stripSize * N, MPI_DOUBLE, 0, TAG, MPI_COMM_WORLD);
- }
-	MPI_Barrier(MPI_COMM_WORLD);
-}
-if (myrank == 0) {
-    endTime = MPI_Wtime();
-    printf("Time is %f\n", endTime-startTime);
-  }
-	
-if(myrank == 0){
-		for(int i=N-1; i >= 0; i--){
-			x[i] = b[i];
-			for(int j=N-1; j > i; j--){
-				x[i] = x[i] - A[i][j] * x[j];
-				}
-			  	x[i] = x[i]/A[i][i];
-		//	printf("x%d = %f\n", i, x[i]);
-			}
-
-	}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	MPI_Finalize();
-	return 0;
-
+   MPI_Scatter(A[0], numElements, MPI_DOUBLE, LocalA[0], numElements, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
 }

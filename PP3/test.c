74d6e69e6dc2c3d5c3ffe3552dc0d6e4ad69bf2f
Jordan Coe <35575950+JuJoCoe@users.noreply.github.com>
2019-05-05 20:27:14 -0500

    Update test.c

--- PP3/test.c
+++ PP3/test.c
@@ -111,7 +111,7 @@ int main(int argc, char **argv)
 	numElements = stripSize;
 
 	
- //  MPI_Scatter(A, numElements, MPI_DOUBLE, LocalA, numElements, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+   MPI_Scatter(A, numElements, MPI_DOUBLE, LocalA[0], numElements, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 	
 	if(myrank == 0){
 	printf("A[0][0] = %f\n", A[1][0]);	
